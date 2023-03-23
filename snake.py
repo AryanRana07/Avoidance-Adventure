094bfc40c0305d021e00ba47a7d94f4759130753
AryanRana-02 <aryanrana0703@gmail.com>
2023-03-23 23:55:04 +0530

    Detect collision

--- snake.py
+++ snake.py
@@ -25,3 +25,15 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 game_over = False
 
 clock = pygame.time.Clock()
+
+def detect_coll(player_pos, enemy_pos):
+	p_x = player_pos[0]
+	p_y = player_pos[1]
+
+	e_x = player_pos[0]
+	e_y = player_pos[1]
+
+	if (e_x >= p_x and e_x < (p_x + player_size)) or (p_x >= e_x and p_x < (e_x + enemy_size)):
+		if(e_y >= p_y and e_y < (p_y + player_size)) or (p_y >= e_y and p_y < (e_y + enemy_size)):
+			return True
+	return False
